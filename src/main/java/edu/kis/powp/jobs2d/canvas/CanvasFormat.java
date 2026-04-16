af0016ae89812883011ce32104088ae7dbbbdfaa
Albert G. <252793@edu.p.lodz.pl>
2026-04-16 14:17:07 +0200

    Created the CanvasFormat interface for defining custom canvas formats. Added PaperFormat for standard paper sizes.

---
+++ src/main/java/edu/kis/powp/jobs2d/canvas/CanvasFormat.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.canvas;
+
+import java.awt.Shape;
+
+public interface CanvasFormat {
+    Shape getShape();
+    String getName();
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/canvas/PaperFormat.java
@@ -0,0 +1,27 @@
+package edu.kis.powp.jobs2d.canvas;
+
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
+
+public enum PaperFormat implements CanvasFormat {
+    A4(210, 297),
+    B3(353, 500);
+
+    private final int width;
+    private final int height;
+
+    PaperFormat(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public Shape getShape() {
+        return new Rectangle2D.Double(0, 0, width, height);
+    }
+
+    @Override
+    public String getName() {
+        return "Paper Format Canvas (" + width + "x" + height + ")";
+    }
+}
